45bd8013d19af961d8095e6a1a846300ae362eed
Lucstay11 <89875785+Lucstay11@users.noreply.github.com>
2023-10-26 18:05:04 +0200

    Add files via upload

--- api/api.js
+++ api/api.js
@@ -1,1 +1,16 @@
+export const cityPrevision = async (lat,long) =>{
+	const TOKEN = "0ce0e533d4fa99e7121f6c5af857c68e"
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${TOKEN}&lang=fr&units=metric`;
+    const options = {
+	method: 'GET',
+    };
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	return result;
+} catch (error) {
+	console.error(error);
+}
+}
 
